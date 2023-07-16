7c3ed65ac73ce49947e76bb54046e622464bcacc
Piyush <40383768+Piyushhhhh@users.noreply.github.com>
2023-07-16 16:18:55 +0530

    added test cases

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,66 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:medical_consultation/main.dart';
+import 'package:medical_consultation/features/conversations/provider/conversation_provider.dart';
+import 'package:medical_consultation/features/dashboard/provider/dashboard_provider.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  //dashboard tests
+  group('DashboardProvider Tests', () {
+    late DashboardProvider dashboardProvider;
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    setUp(() {
+      dashboardProvider = DashboardProvider();
+    });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    test('Initialization Test', () async {
+      await dashboardProvider.init();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+      // Assert that topRecomendedDoctor and userCurrentAppointment lists are not empty after initialization.
+      expect(dashboardProvider.topRecomendedDoctor.isNotEmpty, true);
+      expect(dashboardProvider.userCurrentAppointment.isNotEmpty, true);
+    });
+
+    test('Fetch Recommended Doctor Test', () async {
+      await dashboardProvider.fetchRecomendedDoctor();
+
+      // Assert that topRecomendedDoctor list is not empty after fetching.
+      expect(dashboardProvider.topRecomendedDoctor.isNotEmpty, true);
+    });
+
+    test('Fetch User Appointments Test', () async {
+      await dashboardProvider.userAppointments();
+
+      // Assert that userCurrentAppointment list is not empty after fetching.
+      expect(dashboardProvider.userCurrentAppointment.isNotEmpty, true);
+    });
+
+    test('Switch Doctor Description Text Extended Test', () {
+      // Assert that the initial value of isDoctorDescriptionTextExtended is true.
+      expect(dashboardProvider.isDoctorDescriptionTextExtended, true);
+
+      // Call the switchDoctorDescriptionTextExtended function and check if the value has changed.
+      dashboardProvider.switchDoctorDescriptionTextExtended();
+      expect(dashboardProvider.isDoctorDescriptionTextExtended, false);
+
+      // Call the switchDoctorDescriptionTextExtended function again and check if the value has changed back to true.
+      dashboardProvider.switchDoctorDescriptionTextExtended();
+      expect(dashboardProvider.isDoctorDescriptionTextExtended, true);
+    });
+  });
+
+//conversation  test cases
+
+  group('ConversationProvider Tests', () {
+    late ConversationProvider conversationProvider;
+
+    setUp(() {
+      conversationProvider = ConversationProvider();
+    });
+
+    test('Fetch User Current Conversations Test', () async {
+      await conversationProvider.featchUserCurrentConversation();
+
+      // Assert that userCurrentConversations list is not empty after fetching.
+      expect(conversationProvider.userCurrentConversations.isNotEmpty, true);
+    });
   });
 }
